eaa7bc7764647396a31b58abb84bd629c4874f4f
mavolin <48887425+mavolin@users.noreply.github.com>
2020-04-27 02:28:52 +0200

    fix(logstract): add missing ...

--- pkg/logstract/logger.go
+++ pkg/logstract/logger.go
@@ -36,70 +36,70 @@ func (l *Logger) WithFields(f Fields) *Entry {
 // Debug creates a debug-level log entry, using the passed message.
 func (l *Logger) Debug(a ...interface{}) {
 	if l != nil {
-		l.l(LvlDebug, fmt.Sprint(a), nil)
+		l.l(LvlDebug, fmt.Sprint(a...), nil)
 	}
 }
 
 // Debugf creates a debug-level log entry, by formatting the passed message.
 func (l *Logger) Debugf(f string, a ...interface{}) {
 	if l != nil {
-		l.l(LvlDebug, fmt.Sprintf(f, a), nil)
+		l.l(LvlDebug, fmt.Sprintf(f, a...), nil)
 	}
 }
 
 // Info creates a info-level log entry, using the passed message.
 func (l *Logger) Info(a ...interface{}) {
 	if l != nil {
-		l.l(LvlInfo, fmt.Sprint(a), nil)
+		l.l(LvlInfo, fmt.Sprint(a...), nil)
 	}
 }
 
 // Infof creates a info-level log entry, by formatting the passed message.
 func (l *Logger) Infof(f string, a ...interface{}) {
 	if l != nil {
-		l.l(LvlInfo, fmt.Sprintf(f, a), nil)
+		l.l(LvlInfo, fmt.Sprintf(f, a...), nil)
 	}
 }
 
 // Warn creates a warning-level log entry, using the passed message.
 func (l *Logger) Warn(a ...interface{}) {
 	if l != nil {
-		l.l(LvlWarn, fmt.Sprint(a), nil)
+		l.l(LvlWarn, fmt.Sprint(a...), nil)
 	}
 }
 
 // Warnf creates a warning-level log entry, by formatting the passed message.
 func (l *Logger) Warnf(f string, a ...interface{}) {
 	if l != nil {
-		l.l(LvlWarn, fmt.Sprintf(f, a), nil)
+		l.l(LvlWarn, fmt.Sprintf(f, a...), nil)
 	}
 }
 
 // Error creates a error-level log entry, using the passed message.
 func (l *Logger) Error(a ...interface{}) {
 	if l != nil {
-		l.l(LvlError, fmt.Sprint(a), nil)
+		l.l(LvlError, fmt.Sprint(a...), nil)
 	}
 }
 
 // Errorf creates a error-level log entry, by formatting the passed message.
 func (l *Logger) Errorf(f string, a ...interface{}) {
 	if l != nil {
-		l.l(LvlError, fmt.Sprintf(f, a), nil)
+		l.l(LvlError, fmt.Sprintf(f, a...), nil)
 	}
 }
 
 // Fatal creates a fatal-level log entry, using the passed message.
 func (l *Logger) Fatal(a ...interface{}) {
 	if l != nil {
-		l.l(LvlFatal, fmt.Sprint(a), nil)
+		l.l(LvlFatal, fmt.Sprint(a...), nil)
 	}
 }
 
 // Fatalf creates a fatal-level log entry, by formatting the passed message.
 func (l *Logger) Fatalf(f string, a ...interface{}) {
 	if l != nil {
-		l.l(LvlFatal, fmt.Sprintf(f, a), nil)
+		l.l(LvlFatal, fmt.Sprintf(f, a...), nil)
 	}
 }
 
@@ -144,69 +144,69 @@ func (e *Entry) WithFields(f Fields) *Entry {
 // Debug creates a debug-level log entry, using the passed message.
 func (e *Entry) Debug(a ...interface{}) {
 	if e != nil {
-		e.l(LvlDebug, fmt.Sprint(a), nil)
+		e.l(LvlDebug, fmt.Sprint(a...), nil)
 	}
 }
 
 // Debugf creates a debug-level log entry, by formatting the passed message.
 func (e *Entry) Debugf(f string, a ...interface{}) {
 	if e != nil {
-		e.l(LvlDebug, fmt.Sprintf(f, a), nil)
+		e.l(LvlDebug, fmt.Sprintf(f, a...), nil)
 	}
 }
 
 // Info creates a info-level log entry, using the passed message.
 func (e *Entry) Info(a ...interface{}) {
 	if e != nil {
-		e.l(LvlInfo, fmt.Sprint(a), nil)
+		e.l(LvlInfo, fmt.Sprint(a...), nil)
 	}
 }
 
 // Infof creates a info-level log entry, by formatting the passed message.
 func (e *Entry) Infof(f string, a ...interface{}) {
 	if e != nil {
-		e.l(LvlInfo, fmt.Sprintf(f, a), nil)
+		e.l(LvlInfo, fmt.Sprintf(f, a...), nil)
 	}
 }
 
 // Warn creates a warning-level log entry, using the passed message.
 func (e *Entry) Warn(a ...interface{}) {
 	if e != nil {
-		e.l(LvlWarn, fmt.Sprint(a), nil)
+		e.l(LvlWarn, fmt.Sprint(a...), nil)
 	}
 }
 
 // Warnf creates a warning-level log entry, by formatting the passed message.
 func (e *Entry) Warnf(f string, a ...interface{}) {
 	if e != nil {
-		e.l(LvlWarn, fmt.Sprintf(f, a), nil)
+		e.l(LvlWarn, fmt.Sprintf(f, a...), nil)
 	}
 }
 
 // Error creates a error-level log entry, using the passed message.
 func (e *Entry) Error(a ...interface{}) {
 	if e != nil {
-		e.l(LvlError, fmt.Sprint(a), nil)
+		e.l(LvlError, fmt.Sprint(a...), nil)
 	}
 }
 
 // Errorf creates a error-level log entry, by formatting the passed message.
 func (e *Entry) Errorf(f string, a ...interface{}) {
 	if e != nil {
-		e.l(LvlError, fmt.Sprintf(f, a), nil)
+		e.l(LvlError, fmt.Sprintf(f, a...), nil)
 	}
 }
 
 // Fatal creates a fatal-level log entry, using the passed message.
 func (e *Entry) Fatal(a ...interface{}) {
 	if e != nil {
-		e.l(LvlFatal, fmt.Sprint(a), nil)
+		e.l(LvlFatal, fmt.Sprint(a...), nil)
 	}
 }
 
 // Fatalf creates a fatal-level log entry, by formatting the passed message.
 func (e *Entry) Fatalf(f string, a ...interface{}) {
 	if e != nil {
-		e.l(LvlFatal, fmt.Sprintf(f, a), nil)
+		e.l(LvlFatal, fmt.Sprintf(f, a...), nil)
 	}
 }
